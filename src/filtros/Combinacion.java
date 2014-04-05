5d06adf9dc5986db92ad7a77e9e6507fac1c11a6
Oscar Hernández <chesscon@gmail.com>
2014-04-05 00:34:20 -0600

    Correcciones al metodo que reduce una imagen (usando la técnica de foto mosaico). Se termina el metodo blending (para fusionar 2 imagenes)

--- src/filtros/Combinacion.java
+++ src/filtros/Combinacion.java
@@ -3,8 +3,12 @@ package filtros;
 
 import base.Manipulacion;
 import static base.Manipulacion.cargarImagen;
+import static base.Manipulacion.modificarDimensiones;
+import base.ProImageException;
 import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
 import java.io.IOException;
+import static transformaciones.TamanioImg.reducir;
 
 /**
  *
@@ -12,14 +16,154 @@ import java.io.IOException;
  */
 public class Combinacion extends Manipulacion {
     
-    public static BufferedImage aplicarMatriz(String name, String name2 ) 
-            throws IOException {
+    /**
+     * Combina dos imagenes de manera vertical (una a lado de la otra).
+     * @param nameFile1 Nombre de la imagen que irá a la izquierda
+     * @param nameFile2 Nombre de la imagen que irá a la derecha
+     * @param nAncho ancho en pixeles que se desea para la imagen combinada 
+     * (debe ser menor al ancho del minimo ancho de las dos imágenes)
+     * @param nAlto alto en pixeles que se desea para la imagen combinada (debe
+     * se menor al alto del mínimo alto de las dos imágenes)
+     * @return El buffer con la imagen que resulta de combinar las 2 solicitadas
+     * @throws IOException Se lanza si ocurre un error al intentar leer la imagen
+     * @throws ProImageException Se lanza si las dimensiones especificas no se
+     * pueden utilizar. (ya que son mayores a alguna de las imágenes)
+     */
+    public static BufferedImage combinarVertical(String nameFile1, 
+            String nameFile2, int nAncho,int nAlto) throws IOException, ProImageException {        
+        int nAncho1 = nAncho/2;
+        int nAncho2 = nAncho - nAncho1;
+        BufferedImage img1 = reducir(nameFile1, nAncho1, nAlto);
+        BufferedImage img2 = reducir(nameFile2, nAncho2, nAlto);
+        
+        modificarDimensiones(nAncho, nAlto);
+       
+        Raster r1 = img1.getData();
+        Raster r2 = img2.getData();
+        
+        int pixel[] = null;
+        
+        for (int i = 0; i < nAncho1; i++) {
+            for (int j = 0; j < nAlto; j++) {
+                dstImg.setPixel(i, j, r1.getPixel(i, j, pixel)); 
+            }
+        }
+        
+        for (int i = nAncho1; i < nAncho; i++) {
+            for (int j = 0; j < nAlto; j++) {
+                dstImg.setPixel(i, j, r2.getPixel(i-nAncho1, j, pixel)); 
+            }
+        }
+        
+        buffOutImg.setData(dstImg);
+        return buffOutImg;
+    }
+    
+    /**
+     * Combina dos imagenes de manera horizontal (una a bajo de la otra).
+     * @param nameFile1 Nombre de la imagen que irá arriba
+     * @param nameFile2 Nombre de la imagen que irá abajo
+     * @param nAncho ancho en pixeles que se desea para la imagen combinada 
+     * (debe ser menor al ancho del minimo ancho de las dos imágenes)
+     * @param nAlto alto en pixeles que se desea para la imagen combinada (debe
+     * se menor al alto del mínimo alto de las dos imágenes)
+     * @return El buffer con la imagen que resulta de combinar las 2 solicitadas
+     * @throws IOException Se lanza si ocurre un error al intentar leer la imagen
+     * @throws ProImageException Se lanza si las dimensiones especificas no se
+     * pueden utilizar. (ya que son mayores a alguna de las imágenes)
+     */
+    public static BufferedImage combinarHorizontal(String nameFile1, 
+            String nameFile2, int nAncho,int nAlto) throws IOException, ProImageException {        
+        int nAlto1 = nAlto/2;
+        int nAlto2 = nAlto - nAlto1;
+        BufferedImage img1 = reducir(nameFile1, nAncho, nAlto1);
+        BufferedImage img2 = reducir(nameFile2, nAncho, nAlto2);
+        
+        modificarDimensiones(nAncho, nAlto);
+       
+        Raster r1 = img1.getData();
+        Raster r2 = img2.getData();
+        
+        int pixel[] = null;
+        
+        for (int i = 0; i < nAncho; i++) {
+            for (int j = 0; j < nAlto1; j++) {
+                dstImg.setPixel(i, j, r1.getPixel(i, j, pixel)); 
+            }
+        }
+        
+        for (int i = 0; i < nAncho; i++) {
+            for (int j = nAlto1; j < nAlto; j++) {
+                dstImg.setPixel(i, j, r2.getPixel(i, j-nAlto1, pixel)); 
+            }
+        }
+        
+        buffOutImg.setData(dstImg);
+        return buffOutImg;
+    }
+    
+    
+    /**
+     * Realiza el blending entre 2 imágenes (las fusiona de acuerdo al 
+     * porcentaje especificado)
+     * @param name Nombre del archivo que contiene la imagen 1
+     * @param name2 Nombre del archivo que contiene la imagen 2
+     * @param p porcentaje en que predominará la imagen1
+     * @return regresa un buffer con el blending de las 2 imágenes
+     * @throws IOException Se lanza si ocurre un error mientras se lee la imagen
+     * @throws base.ProImageException
+     */
+    public static BufferedImage blending(String name, String name2, double p ) 
+            throws IOException, ProImageException {
+        p = p > 1 ? 1 : p;
+        p = p < 0 ? 0 : p;
         cargarImagen(name);
+        int ancho1 = anchoImg;
+        int alto1 = altoImg;
+        
+        System.out.println("Blending.....");
+        System.out.println("Dimensiones Imagen1: " + ancho1 + " , " + alto1);
         BufferedImage img1 = buffImg;
         cargarImagen(name2);
-        //BufferedImage img2 = buffImg;
+        int ancho2 = anchoImg;
+        int alto2 = altoImg;
+        System.out.println("Dimensiones Imagen2: " + ancho2 + " , " + alto2);
+        BufferedImage img2 = buffImg;
         
-        return null;
+        int nAncho = Math.min(ancho1, ancho2);
+        int nAlto = Math.min(alto1, alto2);
+        System.out.println("Dimensiones Imagen a generar: " + nAncho + " , " + nAlto);
+        img1 = reducir(img1, nAncho, nAlto);
+        img2 = reducir(img2, nAncho, nAlto);
+        
+        Raster r1 = img1.getData();
+        Raster r2 = img2.getData();
+        
+        int pixel[] = null;
+        double red, green, blue;
+        double q = 1-p;
+        for (int i = 0; i < nAncho; i++) {
+            for (int j = 0; j < nAlto; j++) {
+                red = 0;
+                green = 0;
+                blue = 0;
+                pixel = r1.getPixel(i, j, pixel);
+                red += pixel[0]*p;
+                green += pixel[1]*p;
+                blue += pixel[2]*p;
+                pixel = r2.getPixel(i, j, pixel);
+                red += pixel[0]*q;
+                green += pixel[1]*q;
+                blue += pixel[2]*q;
+                pixel[0] = (int) red;
+                pixel[1] = (int) green;
+                pixel[2] = (int) blue;
+                dstImg.setPixel(i, j, pixel); 
+            }
+        }
+        
+        buffOutImg.setData(dstImg);
+        return buffOutImg;
     }
     
 }
--- src/transformaciones/TamanioImg.java
+++ src/transformaciones/TamanioImg.java
@@ -16,7 +16,7 @@ public class TamanioImg extends Manipulacion {
     
     /**
      * Reduce el tamaño de una imagen, de acuerdo a los parámetros especificados
-     * @param nameFile Nombre del archivo de la imagen que se desea reducir
+     * @param buffImage buffer del archivo de la imagen que se desea reducir
      * @param nAncho Ancho en pixeles al cual se desea recucir
      * @param nAlto Alto en pixeles al cual se desea reducir
      * @return el buffer que contiene la imagen reducida.
@@ -29,37 +29,79 @@ public class TamanioImg extends Manipulacion {
         cargarImagen(buffImage);
         nAlto = Math.abs(nAlto);
         nAncho = Math.abs(nAncho);
+        
         if (nAlto > altoImg || nAncho > anchoImg) {
             String error = "El ancho y el alto para reducir deben ser menores "
                     + "que el ancho y alto originales, y mayores que cero.";
             throw new ProImageException(error);
         }
+        
+        System.out.println("Xn: " +  nAncho);
+        System.out.println("Yn: " + nAlto);
+        
+        int anchoOri = anchoImg;
+        int altoOri = altoImg;
+        
+        double proporX = anchoOri/(double)nAncho;
+        double proporY = altoOri/(double)nAlto;
+        
+        int desplazamientoX = (int) Math.floor(proporX);
+        int desplazamientoY = (int) Math.floor(proporY);
+        
+        int proporcionX = (int) Math.ceil(proporX);
+        int proporcionY = (int) Math.ceil(proporY);
+        
+        System.out.println("xOri: " +  anchoOri);
+        System.out.println("yOri: " + altoOri);
+        
+        System.out.println("orig/nue X:" + anchoOri/(double)nAncho);
+        System.out.println("orig/nue Y:" + altoOri/(double)nAlto);
+        
+        System.out.println("ceil_x: " +  Math.ceil(anchoOri/(double)nAncho));
+        System.out.println("ceil_y: " + Math.ceil(altoOri/(double)nAlto));
+        
+        System.out.println("floor_x: " +  Math.floor(anchoOri/(double)nAncho));
+        System.out.println("floor_y: " + Math.floor(altoOri/(double)nAlto));
+        
+        int propX = (int) Math.ceil(proporX);
+        int propY = (int) Math.ceil(proporY);
+        System.out.println("PropX: " +  propX);
+        System.out.println("PropY: " + propY);
+        
+        Raster r1 = buffImg.getData();
+        
         modificarDimensiones(nAncho, nAlto);
+        
         int [] pixel = null;
         int cant;
         int red, green, blue;
-        
-        int proporX = anchoImg/nAncho;
-        int proporY = altoImg/nAlto;
-        
-        for (int i = 0, in = 0; i < anchoImg && in< nAncho; i+=proporX, in++) {
-            for (int j = 0, jn = 0; j < altoImg && jn < nAlto; j+=proporY, jn++) {
+        double i = 0, j = 0;
+        int in = 0, jn = 0;
+        for (i = 0, in = 0; in < nAncho && i < anchoOri; i+=proporX, in++) {
+            for (j = 0, jn = 0; j < altoOri && jn < nAlto; j+=proporY, jn++) {
                 red = 0;
                 green = 0;
                 blue = 0;
                 cant = 0;
-                for (int i2 = 0; i2 < proporX && (i2+i) < anchoImg; i2++) {
-                    for (int j2 = 0; j2 < proporY && (j2+j) < altoImg; j2++) {
-                        pixel = rasterImg.getPixel(i2+i, j2+j, pixel);
+                
+                int aux1 = (int) Math.floor(i);
+                int aux2 = (int) Math.floor(j);
+                
+                for (int i2 = 0; i2 < proporcionX && (i2+aux1) < anchoOri; i2++) {
+                    for (int j2 = 0; j2 < proporcionY && (j2+aux2) < altoOri; j2++) {
+                        pixel = r1.getPixel(i2+aux1, j2+aux2, pixel);
                         red += pixel[0];
                         green += pixel[1];
                         blue += pixel[2];
                         cant++;
                     }
                 }
-                red = red / cant;
-                green = green / cant;
-                blue = blue / cant;
+                if (cant!= 0) {
+                    red /= cant;
+                    green /= cant;
+                    blue /= cant;
+                }
+
                 dstImg.setSample(in, jn, 0, red); 
                 dstImg.setSample(in, jn, 1, green); 
                 dstImg.setSample(in, jn, 2, blue); 
