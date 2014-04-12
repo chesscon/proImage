5218348ecfbd0fd85654775a39726ebe375c5b79
Oscar Hernández <chesscon@gmail.com>
2014-04-11 23:59:04 -0500

    Se agrega el controlador para el menu de Filtros.

---
+++ src/gui/ListenerMenuFiltros.java
@@ -0,0 +1,23 @@
+
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Controlador para el menu de los filtros
+ * @author oscahern
+ */
+class ListenerMenuFiltros implements ActionListener {
+  public static final String MENU_FILTROS = "Filtros" ;
+
+  /**
+   * Controlador para los eventos sobre este menú
+   * @param e el evento que provoco la llamada a este controlador
+   */
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+  
+}
