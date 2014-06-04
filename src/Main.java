942be2f3b3b1a9d024d9acec9375a37129daeab7
Oscar Hdz C <chesscon@gmail.com>
2014-06-03 22:09:11 -0500

    Se crea proyecto de Netbeans, y se agrega el archivo Main para ejecutar las pruebas con la interfaz

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import gui.ProImageFrame;
+
+
+/**
+ *
+ * @author oscahern
+ */
+public class Main {
+    public static void main(String args[]) {
+        ProImageFrame frame = new ProImageFrame();
+        frame.setVisible(true);
+    }
+}
