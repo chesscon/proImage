dddf8f3eac4052fa179f2f016c3b16c56366062c
Oscar Hdz C <chesscon@gmail.com>
2014-06-03 22:08:01 -0500

    Se añaden los paneles para mostrar las imagenes, con sus secciones para mostrar texto. Ademas se agrega el panel para herramientas

--- src/gui/ProImageFrame.java
+++ src/gui/ProImageFrame.java
@@ -2,14 +2,21 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.io.File;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import javax.swing.border.LineBorder;
 
 /**
  * Frame para la interfaz gráfica.
@@ -41,12 +48,22 @@ public class ProImageFrame extends JFrame {
   /**
     * Panel en donde se cargará la imagen origen
     */
-  private JPanel panelImgSource;
+  private JPanelImagen panelImgSource;
     
   /**
    * Panel en donde se mostrará la imagen modificada
    */
-  private JPanel panelImgTarget;
+  private JPanelImagen panelImgTarget;
+  
+  /**
+   * Componente en donde se mostrará información (texto) de la imagen modificada
+   */
+  private JLabel lblImgTgt;
+  
+  /**
+   * Componente en donde se mostrará información (texto) de la imagen original
+   */
+  private JLabel lblImgSrc;
     
   /**
    * Panel para las herramientas (controles, botones) adicionales de cada filtro
@@ -57,6 +74,9 @@ public class ProImageFrame extends JFrame {
    * Constructor que inicializa los valores por defecto para el frame
    */
   public ProImageFrame() {
+    super (NAME);
+    Container container = this.getContentPane();
+    container.setLayout(new GridBagLayout());
     // Definimos las dimensiones del frame
     this.setSize(ANCHO, ALTO);
     
@@ -71,15 +91,88 @@ public class ProImageFrame extends JFrame {
     
     // Colocar la ventana en el centro e la pantalla
     this.setLocationRelativeTo(null);
-        
-    // Definimos el layout por defecto:
-    BorderLayout layout = new BorderLayout();
-    layout.setHgap(10);
-    layout.setVgap(5);
-        
-    this.setLayout(layout);
     
     // ---  Asignamos los componentes del frame: ----------------
+    
+    GridBagConstraints constraints = new GridBagConstraints();
+    
+    // Texto imagen original
+    lblImgSrc = new JLabel("Imagen original: ");
+    lblImgSrc.setSize(10, 50);
+    constraints.gridx = 0; 
+    constraints.gridy = 0; 
+    //constraints.anchor = GridBagConstraints.EAST; 
+    constraints.gridwidth = 1; 
+    constraints.gridheight = 1;
+    constraints.fill = GridBagConstraints.BOTH;     
+    constraints.weighty = 0.1;
+    constraints.weightx = 1.0;
+    constraints.insets = new Insets(3,3,3,3);    
+    container.add (lblImgSrc, constraints);
+    
+    // panel imagen original
+    panelImgSource = new JPanelImagen();
+    panelImgSource.setImagen("imgs/Pinguino.jpg");
+    panelImgSource.setMinimumSize(new Dimension(ANCHO - 10, ALTO-10));
+    panelImgSource.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+    panelImgSource.setOpaque(false);
+    constraints.gridx = 0; // La imagen original empieza en la columna cero.
+    constraints.gridy = 1; // La imagen original empieza en la fila uno
+    //constraints.anchor = GridBagConstraints.CENTER; 
+    constraints.gridwidth = 1; // La imagen ocupa una columna
+    constraints.gridheight = 1; // La imagen ocupa una fila
+    constraints.fill = GridBagConstraints.BOTH;     
+    constraints.weighty = 1.0;
+    constraints.weightx = 1.0;
+    constraints.insets = new Insets(3,3,3,3);    
+    container.add (panelImgSource, constraints);
+    
+    // texto imagen modificada
+    lblImgTgt = new JLabel("Imagen modificada: ");
+    lblImgTgt.setSize(10, 50);
+    constraints.gridx = 1; 
+    constraints.gridy = 0; 
+    constraints.gridwidth = 1; 
+    constraints.gridheight = 1;
+    constraints.fill = GridBagConstraints.BOTH;     
+    constraints.weighty = 0.1;
+    constraints.weightx = 1.0;
+    constraints.insets = new Insets(3,3,3,3);    
+    container.add (lblImgTgt, constraints);
+    
+    // Agregamos el panel que contendrá la imagen modificada
+    panelImgTarget = new JPanelImagen();
+    panelImgTarget.setImagen("imgs/Pinguino.jpg");
+    panelImgTarget.setMinimumSize(new Dimension(ANCHO - 10, ALTO-50));
+    panelImgTarget.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+    panelImgTarget.setOpaque(false);
+        
+    constraints.gridx = 1; // La imagen modificada empieza en la columna uno.
+    constraints.gridy = 1; // La imagen modificada empieza en la fila uno.
+    //constraints.anchor = GridBagConstraints.CENTER; 
+    constraints.gridwidth = 1; // La imagen ocupa una columna
+    constraints.gridheight = 1; // La imagen ocupa una fila
+    constraints.fill = GridBagConstraints.BOTH; 
+    constraints.weighty = 1.0;
+    constraints.weightx = 1.0;
+    constraints.insets = new Insets(3,3,3,3);
+    container.add (panelImgTarget, constraints);
+
+    // panelHerramientas
+    panelHerramientas = new JPanelImagen();
+    panelHerramientas.setMinimumSize(new Dimension(ANCHO - 10, 30));
+    panelHerramientas.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+    panelHerramientas.setOpaque(false);        
+    constraints.gridx = 0;
+    constraints.gridy = 2;
+    //constraints.anchor = GridBagConstraints.WEST; 
+    constraints.gridwidth = 2; // panel herramientas ocupa 2 columnas
+    constraints.gridheight = 1; // y una fila
+    constraints.fill = GridBagConstraints.BOTH; 
+    constraints.weighty = 0.2;
+    constraints.weightx = 1.0;
+    constraints.insets = new Insets(3,3,3,3);
+    container.add (panelHerramientas, constraints);
     
     // Barra de menus
     initBarMenus();
